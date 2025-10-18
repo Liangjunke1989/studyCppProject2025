7fcf19ca460d0aa1641893fd3c26b8055fe24c4f
liangjunke <liangjunke198909@outlook.com>
2025-10-18 15:34:26 +0800

    testCpp_20251018

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Hello, World!" << endl;//cout: c output
+    cout << "\tI am learning cpp01" << endl; //endl: new line
+    cout << "\tI am learning cpp02\n";
+    //return 0;
+}
