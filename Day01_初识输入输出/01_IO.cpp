c1c858f2ad68629f7b3979091e0464e3fb94ef64
liangjunke <liangjunke198909@outlook.com>
2025-10-22 09:27:24 +0800

    ljk_studyCpp_Day01_20251022

--- Day01_初识输入输出/01_IO.cpp
+++ Day01_初识输入输出/01_IO.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
 using namespace std;
-int main() {
+// 第一个测试函数
+void print_hello() {
     cout << "Hello, World!" << endl; //cout: c output
+}
+void testIO();//函数声明
+int main() {
+    print_hello();
+    testIO();
+}
+//测试输入输出
+void testIO() {//函数定义
     int age;
+    string name;
+    cout << "Please enter your name: ";
+    cin >> name;
     cout << "Please enter your age: ";
     cin >> age;
+    cout << "Your name is: " << name << "，\t";
     cout << "Your age is: " << age << endl;
+
 }
+
